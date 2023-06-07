cd0d6fd3d98a17eb1eee62c6bf71296dcb6c174d
mohsin6862 <mohsinctg2206@gmail.com>
2023-06-07 18:44:19 +0600

    Patch method added

--- index.js
+++ index.js
@@ -54,7 +54,24 @@ async function run() {
       const result = await usersCollection.insertOne(user)
       res.send(result)
     })
-    
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
 
           // menu API's
      app.get('/menu',async(req,res)=>{
