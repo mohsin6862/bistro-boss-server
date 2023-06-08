293907c4918441a4c22635af23c441d65bf0df1f
mohsin6862 <mohsinctg2206@gmail.com>
2023-06-08 19:54:23 +0600

    JWT verify added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,22 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
+
+const verifyJWT = (req,res,next)=>{
+  const authorization = req.headers.authorization
+  if (!authorization){
+    return res.status(401).send({error:true, message: 'Unauthorized Access'})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    req.decoded = decoded;
+   next()
+  })
+  
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@mohsin.hrlaneq.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,7 +51,13 @@ async function run() {
      const reviewCollection = client.db("bistroBossBD").collection("reviewDB")
      const usersCollection = client.db("bistroBossBD").collection("usersDB")
 
+    // jWT
 
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+      res.send({token})
+    })
 
     //  User API's
 
@@ -89,10 +112,16 @@ async function run() {
      
 
     //  cart API's
-     app.get('/cart',async(req,res)=>{
+     app.get('/cart',verifyJWT,async(req,res)=>{
       const email = req.query.email;
       if(!email){
         res.send([])
+      }
+
+      const decodedEmail = req.decoded.email
+      if(email !== decodedEmail){
+        return res.status(403).send({error:true,message: 'forbidden access'})
+             
       }
 
         const query = { email: email };
